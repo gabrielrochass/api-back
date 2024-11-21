c2279a27c6947bdbf7f9c0c15dce28bc93fcb455
gabrielrochass <120466775+gabrielrochass@users.noreply.github.com>
2024-11-20 22:45:23 -0300

    feat: filtragem dos dados

--- server.js
+++ server.js
@@ -1,7 +1,44 @@
 import express from 'express';
 
-// inicializa o express. o app representa o servidos
+const posts = [
+    {
+        id: 1,
+        descricao: "Uma foto teste",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Gato brincando no jardim",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 3,
+        descricao: "Gato dormindo na cadeira",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 4,
+        descricao: "Gato curioso olhando para a câmera",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 5,
+        descricao: "Gato preto em uma janela",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 6,
+        descricao: "Gato e sua tigela de comida",
+        imagem: "https://placecats.com/millie/300/150"
+    }
+];
+
+
+// inicializa o express. o app representa o servidor (instancia)
 const app = express(); 
+
+app.use(express.json()); // devolver a estrutura em json para o cliente
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 }); 
@@ -10,8 +47,19 @@ app.listen(3000, () => {
 // listen:  função que "sobe" o servidor
 
 // define as rotas
-app.get("/", (req, res) => {
-   res.status(200).send("Tudo funcionando! Seja bem-vindo!"); 
+app.get("/posts", (req, res) => {
+   res.status(200).json(posts); 
+}); 
+
+function buscarPostsPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id); // verifica se bate com aquele id
+    });
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostsPorID(req.params.id); //pega o id da requisição
+    res.status(200).json(posts[index]); 
 }); 
 
 // quando tiver nessa rota, mostra uma coisa determinada
@@ -19,4 +67,6 @@ app.get("/", (req, res) => {
 // res: resposta
 // 200: ok do protocolo http
 // send: função que mostra uma mensagem na página web do localhost
+// :id: vai ser substituído pelo id
+// findIndex: método do javascript para entrar em um array
 
