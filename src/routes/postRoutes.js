cdf90b1bca3c36c11b048a4b0eecb548f1a7b858
gabrielrochass <120466775+gabrielrochass@users.noreply.github.com>
2024-11-22 11:44:15 -0300

    fix: conexão com o front pelo cors

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,8 +1,14 @@
 // camada padrão que tem a responsabilidade de armazenar as rotas e definir qual controller será chamado
 
+import cors from "cors";
 import express from "express";
 import multer from "multer";
 import { atualizarNovoPost, listarPosts, postarPost, uploadImagem } from "../controllers/postsController.js";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
 
 // código para funcionar no windows
 const storage = multer.diskStorage({
@@ -18,6 +24,7 @@ const upload = multer({storage}) // middleware: upload de imagem
 
 const routes = (app) => {
     app.use(express.json()); // devolver a estrutura em json para o cliente
+    app.use(cors(cors(corsOptions)));
 
     app.get("/posts", listarPosts); // não executa, apenas lista o que vai ser usado
     app.post("/posts", postarPost) // criar post
